858aeecc8fbbb00bab9d234ece41e36f05ad6106
kebiBGit <173374658+kebiBGit@users.noreply.github.com>
2026-04-01 21:58:46 +0545

    fix: unused

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,10 +1,12 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
 }
-
+-- return {
+--     "iamcco/markdown-preview.nvim",
+--     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--     build = "cd app && npm install",
+--     init = function()
+--         vim.g.mkdp_filetypes = { "markdown" }
+--     end,
+--     ft = { "markdown" },
+-- }
+--
