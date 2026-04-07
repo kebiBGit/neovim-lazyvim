b5fdabc9f088f8875470781166e3e1954a7b5768
kebiBGit <173374658+kebiBGit@users.noreply.github.com>
2026-04-07 13:56:47 +0545

    tab = 2 and snippets

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -40,5 +40,13 @@ return {
         }),
       })
     end,
+        dependencies = {
+      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+      "saadparwaiz1/cmp_luasnip",
+    },
   },
 }
